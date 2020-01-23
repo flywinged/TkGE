b2be52a15b58de097af83436af2c554672ef9138
Clayton Brown <clayton.b@aptus.aero>
2020-01-22 17:13:38 -0700

    initial resizing canvas attempt

--- GameEngine/game.py
+++ GameEngine/game.py
@@ -13,6 +13,25 @@ from .objects.circle import Circle
 
 from .gameState import GameState
 
+# a subclass of Canvas for dealing with resizing of windows
+class ResizingCanvas(Canvas):
+    def __init__(self,parent,**kwargs):
+        Canvas.__init__(self,parent,**kwargs)
+        self.bind("<Configure>", self.on_resize)
+        self.height = self.winfo_reqheight()
+        self.width = self.winfo_reqwidth()
+
+    def on_resize(self,event):
+        # determine the ratio of old width/height to new width/height
+        wscale = float(event.width)/self.width
+        hscale = float(event.height)/self.height
+        self.width = event.width
+        self.height = event.height
+        # resize the canvas 
+        self.config(width=self.width, height=self.height)
+        # rescale all the objects tagged with the "all" tag
+        self.scale("all",0,0,wscale,hscale)
+
 class Game:
     '''
     A Game object contains all the logic necessary for the game loop.
@@ -28,13 +47,13 @@ class Game:
 
         # Create the tk root and initialize the canvas
         self.root = Tk()
-        self.canvas: Canvas = Canvas(self.root, width = self.width, height = self.height, highlightthickness = 0)
+        self.canvas: Canvas = ResizingCanvas(self.root, width = self.width, height = self.height, highlightthickness = 0)
 
         # Set the background of canvas to black, just so it looks sleeker
         self.canvas.configure(background = '#000')
 
         # Pack the canvas so it can be used
-        self.canvas.pack()
+        self.canvas.pack(expand = True, fill = "both")
 
         # Initialize all the event callbacks
         self.root.bind_all("<Motion>", self.eventCallback)
